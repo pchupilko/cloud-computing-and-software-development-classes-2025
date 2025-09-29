451801344768d119256653552b48a43ad954b8ba
pchupilko <petr.chupilko.231@singimail.rs>
2025-09-29 15:12:32 +0200

    Deleted a temporal file
    Deleted a temporal file that was not needed as a separate file actually

--- some_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /tmp
-git clone "https://github.com/pchupilko/cloud-computing-and-software-development-classes-2025"
-
-cd cloud-computing-and-software-development-classes-2025
-rm -rf /var/www/html/*
-mv ./src/* /var/www/html
